e31c0c81b30fb248a930a7974c1f1f4ea9612b28
acc235-ua <acc235@alu.ua.es>
2024-12-20 12:44:08 +0100

    mejoras en corrección

--- codigo.cpp
+++ codigo.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 int main() {
     string nombre;
-    cout << "Hola, dime tu nombre" << endl;
-    cin >> nombre;
-    
-    if ( (nombre.length() == 1) || (nombre.length() == 0) ){
-        cout << "err: bad name"<<endl;
-        exit(0);
-    }else{
 
-        cout << "Hola, " << nombre << endl;
+    while( (nombre.length() == 1) || (nombre.length() == 2) || (nombre.length() == 0) ){
+        cout << "Hola, dime tu nombre" << endl;
+        cin >> nombre;
+        
+        if ( (nombre.length() == 1) || (nombre.length() == 2) ){
+            cout << "err: bad name"<<endl;
+           // exit(0);
+        }
     }
+
+    cout << "Hola, " << nombre << endl;
+    
     return 0;
 }
