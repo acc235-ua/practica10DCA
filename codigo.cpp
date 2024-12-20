b89e1feaa22455022530a3fe0cebb020d531a2cc
acc235-ua <acc235@alu.ua.es>
2024-12-20 12:56:28 +0100

    bucle jugable

--- codigo.cpp
+++ codigo.cpp
@@ -6,6 +6,8 @@ int main() {
     string nombre;
     int num;
     int aleatorio = 0;
+    string continua = "si"; 
+
     rand(tine(0));
     aleatorio = (rand() % 20) + 1 ;
 
@@ -20,15 +22,23 @@ int main() {
     }
 
     cout << "Hola, " << nombre << endl;
+    while( continua == "si"){
+        cout<<"Pensaré un número del 1 al 20 ,intenta adivinarlo"<< nombre << endl << Número: ;
+        cin >> num;
 
-    cout<<"Pensaré un número del 1 al 20 ,intenta adivinarlo"<< nombre << endl << Número: ;
-    cin >> num;
+        if ( num == aleatorio){
 
-    if ( num == aleatorio){
+            cout<< "Enhorabuena !! has ganado "<< endl;
+            continua = "no";
+        }else{
+            cout <<" Mala suerte, estaba pensando en" <<aleatorio << endl;
+            do{
+                cout << "¿ Volverlo a intentar ? (si/no)" << endl;
+                cin >> continua ;
+            }while( (continua != "si") || (continua != "no" ));
 
-        cout<< "Enhorabuena !! has ganado "<< endl;
-    }else{
-        cout <<" Mala suerte, estaba pensando en" <<aleatorio << endl;
+            
+        }
     }
     
     return 0;
