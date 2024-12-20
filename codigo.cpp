784419e2fc37a604fcd47b806a5abb1c45ffe59f
acc235-ua <acc235@alu.ua.es>
2024-12-20 12:49:45 +0100

    commit con errores

--- codigo.cpp
+++ codigo.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main() {
     string nombre;
+    int num;
+    int aleatorio = 0;
+    rand(tine(0));
+    aleatorio = (rand() % 20) + 1 ;
 
     while( (nombre.length() == 1) || (nombre.length() == 2) || (nombre.length() == 0) ){
         cout << "Hola, dime tu nombre" << endl;
@@ -16,6 +20,9 @@ int main() {
     }
 
     cout << "Hola, " << nombre << endl;
+
+    cout<<"Pensaré un número del 1 al 20 ,intenta adivinarlo"<< nombre << endl << Número: ;
+    cin >> num;
     
     return 0;
 }
