2b636b9bf810bbfb7f11613e4c76e067f8cc18b6
acc235-ua <acc235@alu.ua.es>
2024-12-20 12:41:21 +0100

    comprobar nombre

--- codigo.cpp
+++ codigo.cpp
@@ -6,7 +6,13 @@ int main() {
     string nombre;
     cout << "Hola, dime tu nombre" << endl;
     cin >> nombre;
-    cout << "Hola, " << nombre << endl;
+    
+    if ( (nombre.length() == 1) || (nombre.length() == 0) ){
+        cout << "err: bad name"<<endl;
+        exit(0);
+    }else{
 
+        cout << "Hola, " << nombre << endl;
+    }
     return 0;
 }
